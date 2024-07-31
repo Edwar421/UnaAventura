4a5ae2b383aa3fb8e727b58fe0d07e6f81f0fb12
Edwar421 <421edwar@gmail.com>
2024-07-30 22:57:10 -0500

    Doble Autenticacion
    Creacion de la doble autenticacion y manejo de errores

--- una-aventura-digital/src/Components/Login.jsx
+++ una-aventura-digital/src/Components/Login.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../Styles/Login.css';
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth';
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import appFireBase from '../firebase-config';
 
 const auth = getAuth(appFireBase);
 
 export const Login = () => {
-
   const [register, setRegister] = React.useState(false);
+  const [error, setError] = React.useState('');
 
   const functionAutenticacion = async (e) => {
     e.preventDefault();
@@ -18,16 +18,44 @@ export const Login = () => {
     try {
       if (register) {
         try {
-            await createUserWithEmailAndPassword(auth, email, password);
+          await createUserWithEmailAndPassword(auth, email, password);
+          setError(''); // Limpiar error si el registro fue exitoso
         } catch (error) {
-            alert('Asegurate que la contraseña tenga mas de 8 caracteres');
+          switch (error.code) {
+            case 'auth/email-already-in-use':
+              setError('El correo electrónico ya está en uso por otra cuenta.');
+              break;
+            case 'auth/invalid-email':
+              setError('El correo electrónico no tiene un formato válido.');
+              break;
+            case 'auth/weak-password':
+              setError('La contraseña debe tener al menos 8 caracteres.');
+              break;
+            default:
+              setError('Error en el registro. Por favor, inténtalo de nuevo.');
+          }
         }
       } else {
         try {
-            await signInWithEmailAndPassword(auth, email, password);
-        } catch (error) {   
-            alert('El Correo o la contraseña son incorrectos');
-            return;
+          await signInWithEmailAndPassword(auth, email, password);
+          setError(''); // Limpiar error si el inicio de sesión fue exitoso
+        } catch (error) {
+          switch (error.code) {
+            case 'auth/invalid-email':
+              setError('El correo electrónico no tiene un formato válido.');
+              break;
+            case 'auth/user-disabled':
+              setError('La cuenta del usuario ha sido deshabilitada.');
+              break;
+            case 'auth/user-not-found':
+              setError('No existe un usuario registrado con ese correo.');
+              break;
+            case 'auth/wrong-password':
+              setError('La contraseña es incorrecta.');
+              break;
+            default:
+              setError('Error al iniciar sesión. Por favor, inténtalo de nuevo.');
+          }
         }
       }
       console.log('Autenticación exitosa');
@@ -38,12 +66,25 @@ export const Login = () => {
     }
   }
 
+  const handleGoogleLogin = async () => {
+    const provider = new GoogleAuthProvider();
+    try {
+      const credentials = await signInWithPopup(auth, provider);
+      console.log('Google sign-in successful:', credentials);
+      // Close the login modal if applicable
+      // showMessage("Welcome " + credentials.user.displayName);
+    } catch (error) {
+      console.error('Error during Google sign-in:', error);
+    }
+  };
+
   return (
     <div className='container'>
       <div className="row justify-content-center align-items-center vh-100">
         <div className="col-md-6">
           <div className="login-container">
             <h2 className="text-center">{register ? "Registrarse" : "Iniciar Sesión"}</h2>
+            {error && <div className="alert alert-danger" role="alert">{error}</div>}
             <form onSubmit={functionAutenticacion}>
               <div className="form-group">
                 <label htmlFor="email">Correo Electrónico</label>
@@ -55,6 +96,9 @@ export const Login = () => {
               </div>
               <button type="submit" className="btn btn-primary btn-block">{register ? "Regístrate" : "Inicia Sesión"}</button>
             </form>
+            <button type="button" className="btn btn-info btn-block mt-2" onClick={handleGoogleLogin}>
+              Google
+            </button>
             <p className="text-center mt-3">
               {register ? "Si ya tienes cuenta" : "No tienes cuenta"}
               <button className="btn btn-link" onClick={() => setRegister(!register)}>
@@ -64,7 +108,7 @@ export const Login = () => {
           </div>
         </div>
         <div className="col-md-6 d-none d-md-block img-container">
-          <img src="/Imagenes/Viaje.jpg " alt="Viaje Fotográfico" className="img-fluid" />
+          <img src="/Imagenes/Viaje.jpg" alt="Viaje Fotográfico" className="img-fluid" />
         </div>
       </div>
     </div>
